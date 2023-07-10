71a7a7b0ef00359662967538602eec2c0ee4ab97
Daria Shpak <darya.shpak@gmail.com>
2023-07-10 20:46:51 +0300

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -3,8 +3,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import {Label, Input} from "./Filter.styled"
 
-const Filter = ({ filter, onHandleSearch }) => {
-  
+const Filter = ({filter, onHandleSearch}) => {
   return (
     <Label htmlFor="">
       Search 🔮
